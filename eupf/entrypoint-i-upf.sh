9efa5da4b05b4592229fd9b6ad5b6d39d761b2cf
pirog-spb <arozhkevich@edgecom.ru>
2023-10-19 15:57:57 +0300

    add second psa upf

--- eupf/entrypoint-i-upf.sh
+++ eupf/entrypoint-i-upf.sh
@@ -3,5 +3,7 @@
 #update arp table
 ping psaupf.free5gc.org -c 5
 
+ping psaupf-2.free5gc.org -c 5
+
 # Run app replacing current shell (to preserve signal handling) and forward cmd arguments
 exec /app/bin/eupf "$@"
